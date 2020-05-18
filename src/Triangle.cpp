e1ef278f7f8dd41853b6b0358deab0b4b86608c4
sweetechka <titvs1337@mail.ru>
2020-05-18 11:49:13 +0700

    Added triangle function

---
+++ src/Triangle.cpp
@@ -0,0 +1,9 @@
+#include "Triangle.hpp"
+
+void Triangle::tPrint()
+{
+    cout << "\t"
+         << "Perimetr: " << triangleP << endl;
+    cout << "\t"
+         << "Area: " << triangleA << endl;
+}
