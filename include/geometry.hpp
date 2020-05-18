c4ef3fb315464adc800bdabc114c7951cdd81fe8
sweetechka <titvs1337@mail.ru>
2020-05-18 11:42:39 +0700

    Added head file for geometry function

---
+++ include/geometry.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <locale>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+extern int amountOfFigures;
+extern vector<string> str;
+extern string temp;
+extern string fgname;
+
+extern vector<double> coord;
+extern stringstream el;
+
+extern int i;
+
+void geometry();
